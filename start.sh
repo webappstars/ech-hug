1757bb37e441f147ccb6ee44aac72963f0bdffea
webappstars <141623219+webappstars@users.noreply.github.com>
2025-12-08 13:58:15 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -70,7 +70,7 @@ quicktunnel() {
 
         armv8 | arm64 | aarch64 )
 
-            ECH_URL="https://www.baipiao.eu.org/ech/ech-server-linux-arm64"
+            ECH_URL="https://github.com/webappstars/ech-hug/releases/download/3.0/ech-tunnel-linux-arm64"
 
             OPERA_URL="https://github.com/Snawoot/opera-proxy/releases/latest/download/opera-proxy.linux-arm64"
 
